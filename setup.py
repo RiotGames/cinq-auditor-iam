8fed83dc8318e935875150375d14c8aec6f7a2dc
markofu <markofu@users.noreply.github.com>
2017-11-21 16:43:06 -0800

    Correcting metadata for team name, lic & git url

--- setup.py
+++ setup.py
@@ -33,10 +33,10 @@ setuptools.setup(
     # Metadata for the project
     description='IAM Policy and Role auditor',
     long_description=long_desc,
-    url='https://github.com/CloudInquisitor/cinq-auditor-iam/',
-    author='Riot Games InfoSec',
+    url='https://github.com/RiotGames/cinq-auditor-iam/',
+    author='Riot Games Security',
     author_email='security@riotgames.com',
-    license='MIT',
+    license='Apache 2.0',
     classifiers=[
         # Current project status
         'Development Status :: 4 - Beta',
@@ -46,7 +46,7 @@ setuptools.setup(
         'Intended Audience :: Information Technology',
 
         # License information
-        'License :: OSI Approved :: MIT',
+        'License :: OSI Approved :: Apache 2.0',
 
         # Supported python versions
         'Programming Language :: Python :: 3.5',
